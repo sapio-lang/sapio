fe510b2f3c931442146a966181e8acce46325e25
Jeremy Rubin <j@rubin.io>
2020-04-18 02:49:07 -0700

    Test the (renamed) DNF Compilation results

--- sapio/script/compiler.py
+++ sapio/script/compiler.py
@@ -8,11 +8,11 @@ from sapio.script.opcodes import AllowedOp
 from sapio.script.clause import Clause
 from sapio.script.witnessmanager import WitnessTemplate, WitnessManager
 
-T = TypeVar('T')
+DNF = List[List[Clause]]
 
-CNF = List[List[Clause]]
-class ClauseToCNF:
-    def compile_cnf(self, clause: Clause) -> CNF:
+
+class ClauseToDNF:
+    def compile_cnf(self, clause: Clause) -> DNF:
         while True:
             normalizer = NormalizationPass()
             clause = normalizer.normalize(clause)
@@ -29,14 +29,14 @@ class CNFClauseCompiler:
 class ProgramBuilder:
 
     def compile(self, clause: Clause) -> WitnessManager:
-        cnf: CNF = ClauseToCNF().compile_cnf(clause)
-        n_cases = len(cnf)
+        dnf: DNF = ClauseToDNF().compile_cnf(clause)
+        n_cases = len(dnf)
         witness_manager: WitnessManager = WitnessManager()
         # If we have one or two cases, special case the emitted scripts
         # 3 or more, use a generic wrapper
         if n_cases == 1:
             witness = witness_manager.make_witness(0)
-            witness_manager.program += CNFClauseCompiler().compile(cnf[0], witness)
+            witness_manager.program += CNFClauseCompiler().compile(dnf[0], witness)
             # Hack because the fragment compiler leaves stack empty
             witness_manager.program += CScript([AllowedOp.OP_1])
         elif n_cases == 2:
@@ -45,8 +45,8 @@ class ProgramBuilder:
             wit_0.add(1)
             wit_1.add(0)
             # note order of side effects!
-            branch_a = CNFClauseCompiler().compile(cnf[0], wit_0)
-            branch_b = CNFClauseCompiler().compile(cnf[1], wit_1)
+            branch_a = CNFClauseCompiler().compile(dnf[0], wit_0)
+            branch_b = CNFClauseCompiler().compile(dnf[1], wit_1)
             witness_manager.program = CScript([AllowedOp.OP_IF,
                                                branch_a,
                                                AllowedOp.OP_ELSE,
@@ -60,10 +60,9 @@ class ProgramBuilder:
             # Future work can optimize this by inspecting the sub-branches
             # and sharing code...
 
-
             # Check that the first argument passed is an in range execution path
             # Note the first branch does not subtract one, so we have arg in [0, N)
-            for (idx, cl) in enumerate(cnf):
+            for (idx, cl) in enumerate(dnf):
                 wit = witness_manager.make_witness(idx)
                 wit.add(idx)
                 sub_script = CNFClauseCompiler().compile(cl, wit)
@@ -90,7 +89,7 @@ class ProgramBuilder:
                             AllowedOp.OP_ENDIF,
                             # set up for testing the next clause...
                             AllowedOp.OP_1SUB])
-                elif idx+1 < len(cnf):
+                elif idx + 1 < len(dnf):
                     witness_manager.program += \
                         CScript([AllowedOp.OP_IFDUP,
                                  AllowedOp.OP_NOTIF,
@@ -110,4 +109,3 @@ class ProgramBuilder:
                                  AllowedOp.OP_1])
 
         return witness_manager
-
--- tests/test_script/test_compiler.py
+++ tests/test_script/test_compiler.py
@@ -1,10 +1,35 @@
 import unittest
+from functools import reduce
+from operator import and_, or_
 
+from sapio.script.clause import AfterClause, Variable, AbsoluteTimeSpec
+from sapio.script.compiler import  ClauseToDNF
+import random
+class TestCompiler(unittest.TestCase):
+    def test_clause_to_cnf(self):
+        clauses = [
+            [AfterClause(Variable(str(n), AbsoluteTimeSpec.at_height(n))) for n in range(m*100, (m+1) *100)] for m in range(100)]
+        # shuffle the clauses
+        for clause in clauses:
+            random.shuffle(clause)
+        random.shuffle(clauses)
+        anded = [reduce(and_, group[1:], group[0]) for group in clauses]
+        orred = reduce(or_, anded[1:], anded[0])
+        output = ClauseToDNF().compile_cnf(orred)
+        self.assertSetEqual(frozenset(x.a.assigned_value.time for y in output for x in y),
+                         frozenset(range(0,100**2)), "does not preserve values")
+        self.assertSetEqual(frozenset(frozenset(x.a.assigned_value.time for x in y) for y in output),
+                         frozenset(frozenset(range(m*100, (m+1)*100)) for m in range(100)), "does not preserves clauses")
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
+    def test_clause_to_cnf_random(self):
+        A,B,C,D,E,F,G,H,I,J = [AfterClause(Variable(str(n), AbsoluteTimeSpec.at_height(n))) for n in range(10)]
+        inputs = ((((A | B) & C) | D | E | F) & G & H | I) | J
+        # Checked by Wolfram Alpha...
+        # (A ∧ C ∧ G ∧ H) ∨ (B ∧ C ∧ G ∧ H) ∨ (D ∧ G ∧ H) ∨ (E ∧ G ∧ H) ∨ (F ∧ G ∧ H) ∨ J ∨ K
+        expected = [[A,C,G,H], [B,C,G,H], [D,G,H], [E,G,H], [F,G,H], [J], [I]]
+        output = ClauseToDNF().compile_cnf(inputs)
+        to_set = lambda s: frozenset(frozenset(y.a.assigned_value.time for y in x) for x in s)
+        self.assertSetEqual(to_set(output), to_set(expected), "Computes Correctly")
 
 if __name__ == '__main__':
     unittest.main()
