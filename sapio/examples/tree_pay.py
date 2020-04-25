977a3feff91636af3a97db3748617e6902d05295
Jeremy Rubin <j@rubin.io>
2020-04-25 11:55:47 -0700

    Fix collapsable tree

--- sapio/examples/tree_pay.py
+++ sapio/examples/tree_pay.py
@@ -42,20 +42,19 @@ def libsecp_make_musig(x):
 
 class CollapsibleTree(Contract):
     class Fields:
-        payments: List[Tuple[Amount, PubKey]]
+        payments: List[Tuple[Amount, Contract]]
         radix: int
-    @path(lambda _: AfterClause(Weeks(2)))
+    @path
     def expand(self) -> TransactionTemplate:
         tx = TransactionTemplate()
-        for segment in segment_by_radix(self.payments.assigned_value, self.radix.assigned_value):
-            if len(segment) > self.radix.assigned_value:
-                tx.add_output(
-                               functools.reduce(lambda x, y: x + y, [a for (a, _) in segment], Amount(0)),
-                               CollapsibleTree(payments=segment, radix=self.radix.assigned_value)
-                               )
-            else:
-                for payment in segment:
-                    tx.add_output(payment[0], payment[1])
+        segments = list(segment_by_radix(self.payments.assigned_value, self.radix.assigned_value))
+        if len(segments) == 1:
+            for payment in self.payments.assigned_value:
+                tx.add_output(payment[0], payment[1])
+        else:
+            for segment in segments:
+                amount = functools.reduce(lambda x, y: x + y, [a for (a, _) in segment], Amount(0))
+                tx.add_output(amount, TreePay(payments=segment, radix=self.radix.assigned_value))
         return tx
     def get_musig(self) -> AssignedVariable[PubKey]:
         return AssignedVariable(b"0" * 32, "musig")
