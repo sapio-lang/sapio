3f0e7ad37c8b6d8aa9db70192d6303ae97cd7fa0
Jeremy Rubin <j@rubin.io>
2020-04-26 14:19:06 -0700

    fix flatten type errors

--- sapio/script/flatten_and.py
+++ sapio/script/flatten_and.py
@@ -1,46 +1,80 @@
-from typing import List
+from typing import List, TYPE_CHECKING, Union
 
-from sapio.script.clause import Clause, AndClause, OrClause, SignatureCheckClause, \
-    PreImageCheckClause, CheckTemplateVerifyClause, AfterClause, SatisfiedClause, DNFClause, DNF
-from sapio.util import methdispatch
+from sapio.script.clause import (
+    Clause,
+    AndClause,
+    OrClause,
+    SignatureCheckClause,
+    PreImageCheckClause,
+    CheckTemplateVerifyClause,
+    AfterClause,
+    SatisfiedClause,
+    DNFClause,
+    DNF,
+)
+from functools import singledispatchmethod
 
 
 # Assumes that there is no OR which comes after an AND
 
+
 class FlattenPass:
-    @methdispatch
-    def flatten(self, arg: Clause, or_allowed: bool=True) -> DNF:
+    @singledispatchmethod
+    def flatten(self, arg: Clause, or_allowed: bool = True) -> DNF:
         raise NotImplementedError("Cannot Compile Arg", arg)
 
-
     @flatten.register
-    def flatten_and(self, arg: AndClause, or_allowed=False) -> DNF:
-        l = self.flatten(arg.a, or_allowed)
-        l2 = self.flatten(arg.b, or_allowed)
+    def flatten_and(self, arg: AndClause, or_allowed: bool = False) -> DNF:
+        if TYPE_CHECKING:
+            assert callable(self.flatten)
+        l: DNF = self.flatten(arg.a, or_allowed)
+        l2: DNF = self.flatten(arg.b, or_allowed)
         assert len(l) == 1
         assert len(l2) == 1
         l[0].extend(l2[0])
         return l
 
-
     @flatten.register
-    def flatten_sat(self, arg:SatisfiedClause, or_allowed=False) -> DNF:
+    def flatten_sat(self, arg: SatisfiedClause, or_allowed: bool = False) -> DNF:
         return [[]]
 
     @flatten.register
-    def flatten_or(self, arg: OrClause, or_allowed=True) -> DNF:
+    def flatten_or(self, arg: OrClause, or_allowed: bool = True) -> DNF:
+        if TYPE_CHECKING:
+            assert callable(self.flatten)
         assert or_allowed
-        return self.flatten(arg.a, or_allowed) + self.flatten(arg.b, or_allowed)
+        l : DNF =  self.flatten(arg.a, or_allowed)
+        l2: DNF =  self.flatten(arg.b, or_allowed)
+        return l+l2
+
 
     @flatten.register(AfterClause)
     @flatten.register(CheckTemplateVerifyClause)
     @flatten.register(PreImageCheckClause)
     @flatten.register(SignatureCheckClause)
-    def flatten_after(self, arg, or_allowed=False) -> DNF:
+    def flatten_after(
+        self,
+        arg: Union[
+            AfterClause,
+            CheckTemplateVerifyClause,
+            PreImageCheckClause,
+            SignatureCheckClause,
+        ],
+        or_allowed: bool = False,
+    ) -> DNF:
         return [[arg]]
 
+
+# TODO: Move to a test...
 try:
-    FlattenPass().flatten(AndClause(OrClause(SatisfiedClause(), SatisfiedClause()), OrClause(SatisfiedClause(), SatisfiedClause())))
+    f = FlattenPass()
+    assert callable(f.flatten)
+    f.flatten(
+        AndClause(
+            OrClause(SatisfiedClause(), SatisfiedClause()),
+            OrClause(SatisfiedClause(), SatisfiedClause()),
+        )
+    )
     raise AssertionError("this sanity check should fail")
 except AssertionError:
     pass
