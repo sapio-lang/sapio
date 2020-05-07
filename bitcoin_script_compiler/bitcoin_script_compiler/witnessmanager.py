e8f87f64ac78606fda8cfb6b6da2ea5f95ad9109
Jeremy Rubin <j@rubin.io>
2020-05-06 19:27:55 -0700

    static type witnessmanager

--- bitcoin_script_compiler/bitcoin_script_compiler/witnessmanager.py
+++ bitcoin_script_compiler/bitcoin_script_compiler/witnessmanager.py
@@ -12,8 +12,8 @@ CTVHash = NewType("CTVHash", bytes)
 
 
 class WitnessTemplate:
-    def __init__(self):
-        self.witness = []
+    def __init__(self) -> None:
+        self.witness: List[Union[CScript, int, bytes]] = []
         self.ctv_hash: Optional[CTVHash] = None
 
     def add(self, it: Union[CScript, int, bytes]) -> None:
@@ -21,14 +21,14 @@ class WitnessTemplate:
         self.internal_add(it)
 
     @singledispatchmethod
-    def internal_add(self, it: Union[CScript, bytes]):
+    def internal_add(self, it: Union[CScript, bytes]) -> None:
         self.witness.insert(0, it)
 
     @internal_add.register
-    def _(self, it: int):
+    def _(self, it: int) -> None:
         self.add(CScript([it]))
 
-    def will_execute_ctv(self, ctv: CTVHash):
+    def will_execute_ctv(self, ctv: CTVHash) -> None:
         if self.ctv_hash is not None and ctv != self.ctv_hash:
             raise AssertionError("Two CTV Hashes cannot be in the same witness")
         self.ctv_hash = ctv
@@ -38,29 +38,29 @@ class WitnessManager:
     def __init__(self) -> None:
         self.override_program: Optional[str] = None
         self.program: CScript = CScript()
-        self.witnesses: Dict[Any, WitnessTemplate] = {}
+        self.witnesses: Dict[int, WitnessTemplate] = {}
         self.is_final = False
 
     def to_json(self) -> Dict[str, Any]:
         assert self.is_final
         return {}
 
-    def finalize(self):
+    def finalize(self) -> None:
         self.is_final = True
 
-    def get_witness(self, key) -> List[Any]:
+    def get_witness(self, key: int) -> List[Any]:
         assert self.is_final
         item = self.witnesses[key].witness.copy()
         item.append(self.program)
         return item
 
-    def make_witness(self, key) -> WitnessTemplate:
+    def make_witness(self, key: int) -> WitnessTemplate:
         assert not self.is_final
         assert key not in self.witnesses
         self.witnesses[key] = WitnessTemplate()
         return self.witnesses[key]
 
-    def get_p2wsh_script(self, main=False) -> CScript:
+    def get_p2wsh_script(self, main: bool=False) -> CScript:
         if self.override_program is not None:
             script = segwit_addr.decode("bc" if main else "bcrt", self.override_program)
             return CScript([script[0], bytes(script[1])])
