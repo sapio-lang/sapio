a333e5795facee2c60742b98688eca4d985c1f21
Jeremy Rubin <j@rubin.io>
2020-07-08 16:24:07 -0700

    fix bindings error

--- rust_miniscript/rust_miniscript.py
+++ rust_miniscript/rust_miniscript.py
@@ -9,7 +9,7 @@ lib.make_policy.argtypes = [POINTER(c_char), POINTER(c_size_t), POINTER(POINTER(
 def compile_policy(key: bytes) -> bytes:
     size = c_size_t(0)
     data =  POINTER(c_ubyte)()
-    handle = lib.make_policy(ctypes.c_char_p(b"pk("+key+b")\x00"), byref(size),byref(data))
+    handle = lib.make_policy(ctypes.c_char_p(key + b"\x00"), byref(size),byref(data))
     ret = bytearray(size.value)
     for i in range(size.value):
         ret[i] = data[i]
