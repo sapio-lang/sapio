2823798a0599aedeb7f89198cccb953125557953
Anthony <pyskell@users.noreply.github.com>
2020-08-24 18:58:09 -0400

    broken test

--- sapio_zoo/sapio_zoo/hodl_chicken.py
+++ sapio_zoo/sapio_zoo/hodl_chicken.py
@@ -2,15 +2,14 @@ from typing import Tuple
 
 from bitcoin_script_compiler import Satisfied
 from sapio_bitcoinlib.messages import COutPoint
-from sapio_bitcoinlib.static_types import Amount, PubKey
+from sapio_bitcoinlib.static_types import Amount
 from sapio_compiler import Contract, TransactionTemplate, SignedBy, guarantee, check, require
-from sapio_stdlib.p2pk import P2PK
 
 
 class HodlChicken(Contract):
     class Fields:
-        alice_key: PubKey
-        bob_key: PubKey
+        alice_contract: Contract
+        bob_contract: Contract
         alice_deposit: Amount
         bob_deposit: Amount
         winner_gets: Amount
@@ -28,24 +27,24 @@ class HodlChicken(Contract):
 
     @require
     def alice_is_a_chicken(self):
-        return SignedBy(self.alice_key)
+        return SignedBy(self.alice_contract)
     
     @alice_is_a_chicken
     @guarantee
     def alice_redeem(self) -> TransactionTemplate:
         tx = TransactionTemplate()
-        tx.add_output(self.winner_gets, P2PK(key=self.bob_key))        
-        tx.add_output(self.chicken_gets, P2PK(key=self.alice_key))
-        return tx        
+        tx.add_output(self.winner_gets, self.bob_contract)        
+        tx.add_output(self.chicken_gets, self.alice_contract)
+        return tx
 
     @require
     def bob_is_a_chicken(self):
-        return SignedBy(self.bob_key)
+        return SignedBy(self.bob_contract)
     
     @bob_is_a_chicken
     @guarantee
     def bob_redeem(self) -> TransactionTemplate:
         tx = TransactionTemplate()
-        tx.add_output(self.winner_gets, P2PK(key=self.alice_key))        
-        tx.add_output(self.chicken_gets, P2PK(key=self.bob_key))
+        tx.add_output(self.winner_gets, self.alice_contract)        
+        tx.add_output(self.chicken_gets, self.bob_contract)
         return tx  
--- sapio_zoo/tests/test_zoo/test_hodl_chicken.py
+++ sapio_zoo/tests/test_zoo/test_hodl_chicken.py
@@ -2,24 +2,26 @@ from functools import reduce
 
 from sapio_compiler import *
 from sapio_zoo.hodl_chicken import *
-from sapio_bitcoinlib.address import key_to_p2pkh, key_to_p2wpkh
-from sapio_stdlib.p2pk import P2PK
+from sapio_bitcoinlib.address import key_to_p2pkh, key_to_p2wpkh, script_to_p2wsh
+from sapio_bitcoinlib.script import CScript
+from sapio_zoo.p2pk import PayToSegwitAddress
 import os
 import unittest
 from .testutil import random_k
 from sapio_bitcoinlib.messages import COutPoint
 
-
 class TestHodlChicken(unittest.TestCase):
 
     def test_hodl_chicken(self):
-        alice_key = random_k()
-        bob_key = random_k()
+        alice_script = script_to_p2wsh(CScript([b"Alice's Key Goes Here!"]))
+        bob_script = script_to_p2wsh(CScript([b"Bob's Key Goes Here!"]))
+
+        ar = AmountRange()
 
         hodl_chicken = HodlChicken(
-            alice_key=alice_key, 
-            bob_key=bob_key, 
-            alice_deposit=Sats(100_000_000), 
+            alice_contract=PayToSegwitAddress(amount=ar, address=alice_script), 
+            bob_contract=PayToSegwitAddress(amount=ar, address=bob_script), 
+            alice_deposit=Sats(100_000_000),
             bob_deposit=Sats(100_000_000),
             winner_gets=Sats(50_000_000),
             chicken_gets=Sats(150_000_000))
