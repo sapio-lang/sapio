31c6c6a4b7e741003bedb7a75609687a87d26831
Anthony <pyskell@users.noreply.github.com>
2020-08-25 17:43:59 -0400

    Authorize by pubkey and pay out to contract

--- sapio_zoo/tests/test_zoo/test_hodl_chicken.py
+++ sapio_zoo/tests/test_zoo/test_hodl_chicken.py
@@ -15,12 +15,14 @@ class TestHodlChicken(unittest.TestCase):
     def test_hodl_chicken(self):
         alice_script = script_to_p2wsh(CScript([b"Alice's Key Goes Here!"]))
         bob_script = script_to_p2wsh(CScript([b"Bob's Key Goes Here!"]))
-
-        ar = AmountRange()
+        alice_key = random_k()
+        bob_key = random_k()
 
         hodl_chicken = HodlChicken(
-            alice_contract=PayToSegwitAddress(amount=ar, address=alice_script), 
-            bob_contract=PayToSegwitAddress(amount=ar, address=bob_script), 
+            alice_contract=lambda x: PayToSegwitAddress(amount=AmountRange.of(x), address=alice_script), 
+            bob_contract=lambda x: PayToSegwitAddress(amount=AmountRange.of(x), address=bob_script), 
+            alice_key=alice_key,
+            bob_key=bob_key,
             alice_deposit=Sats(100_000_000),
             bob_deposit=Sats(100_000_000),
             winner_gets=Sats(50_000_000),
