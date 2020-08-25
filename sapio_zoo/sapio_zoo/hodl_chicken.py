5f6d2c82dc189ec502509e1a58ad89362fee6d3e
Anthony <pyskell@users.noreply.github.com>
2020-08-25 17:43:59 -0400

    Authorize by pubkey and pay out to contract

--- sapio_zoo/sapio_zoo/hodl_chicken.py
+++ sapio_zoo/sapio_zoo/hodl_chicken.py
@@ -1,15 +1,17 @@
-from typing import Tuple
+from typing import Tuple, Callable
 
 from bitcoin_script_compiler import Satisfied
 from sapio_bitcoinlib.messages import COutPoint
-from sapio_bitcoinlib.static_types import Amount
+from sapio_bitcoinlib.static_types import Amount, PubKey
 from sapio_compiler import Contract, TransactionTemplate, SignedBy, guarantee, check, require
 
 
 class HodlChicken(Contract):
     class Fields:
-        alice_contract: Contract
-        bob_contract: Contract
+        alice_contract: Callable[[Amount], Contract]
+        bob_contract: Callable[[Amount], Contract]
+        alice_key: PubKey
+        bob_key: PubKey
         alice_deposit: Amount
         bob_deposit: Amount
         winner_gets: Amount
@@ -27,24 +29,24 @@ class HodlChicken(Contract):
 
     @require
     def alice_is_a_chicken(self):
-        return SignedBy(self.alice_contract)
+        return SignedBy(self.alice_key)
     
     @alice_is_a_chicken
     @guarantee
     def alice_redeem(self) -> TransactionTemplate:
         tx = TransactionTemplate()
-        tx.add_output(self.winner_gets, self.bob_contract)        
-        tx.add_output(self.chicken_gets, self.alice_contract)
+        tx.add_output(self.winner_gets, self.bob_contract(self.winner_gets))        
+        tx.add_output(self.chicken_gets, self.alice_contract(self.chicken_gets))
         return tx
 
     @require
     def bob_is_a_chicken(self):
-        return SignedBy(self.bob_contract)
+        return SignedBy(self.bob_key)
     
     @bob_is_a_chicken
     @guarantee
     def bob_redeem(self) -> TransactionTemplate:
         tx = TransactionTemplate()
-        tx.add_output(self.winner_gets, self.alice_contract)        
-        tx.add_output(self.chicken_gets, self.bob_contract)
+        tx.add_output(self.winner_gets, self.alice_contract(self.winner_gets))        
+        tx.add_output(self.chicken_gets, self.bob_contract(self.chicken_gets))
         return tx  
