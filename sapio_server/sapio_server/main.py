1002157e9e332a779d6bd881de1e4b1fc2e7cb25
Anthony <pyskell@users.noreply.github.com>
2020-08-25 18:16:53 -0400

    Cleanup imports in main.py

--- sapio_server/sapio_server/main.py
+++ sapio_server/sapio_server/main.py
@@ -2,10 +2,6 @@ import os
 
 import tornado
 
-import sapio_zoo
-import sapio_zoo.channel
-import sapio_zoo.p2pk
-import sapio_zoo.subscription
 from sapio_bitcoinlib import segwit_addr
 from sapio_bitcoinlib.key import ECKey
 from sapio_bitcoinlib.static_types import Amount, Bitcoin, PubKey, Sats
@@ -17,12 +13,16 @@ from sapio_compiler import (
     TimeSpec,
     Weeks,
 )
+from sapio_bitcoinlib.address import key_to_p2pkh, key_to_p2wpkh, script_to_p2wsh
+from sapio_bitcoinlib.script import CScript
 from sapio_zoo.hodl_chicken import HodlChicken
 from sapio_zoo.p2pk import PayToPubKey, PayToSegwitAddress
 from sapio_zoo.pricebet import PriceOracle
 from sapio_zoo.smarter_vault import SmarterVault
 from sapio_zoo.tree_pay import TreePay
 from sapio_zoo.undo_send import UndoSend2
+from sapio_zoo.channel import BasicChannel
+from sapio_zoo.subscription import auto_pay
 
 from .ws import CompilerWebSocket
 
@@ -38,9 +38,9 @@ def make_app():
 
 
 if __name__ == "__main__":
-    CompilerWebSocket.add_contract("Channel", sapio_zoo.channel.BasicChannel)
-    CompilerWebSocket.add_contract("Pay to Public Key", sapio_zoo.p2pk.PayToPubKey)
-    CompilerWebSocket.add_contract("Subscription", sapio_zoo.subscription.auto_pay)
+    CompilerWebSocket.add_contract("Channel", BasicChannel)
+    CompilerWebSocket.add_contract("Pay to Public Key", PayToPubKey)
+    CompilerWebSocket.add_contract("Subscription", auto_pay)
     CompilerWebSocket.add_contract("TreePay", TreePay)
     CompilerWebSocket.add_contract("Chicken", HodlChicken)
     generate_n_address = [
@@ -49,16 +49,11 @@ if __name__ == "__main__":
     payments = [
         (
             5,
-            sapio_zoo.p2pk.PayToSegwitAddress.create(
-                amount=AmountRange.of(0), address=address
-            ),
+            PayToSegwitAddress.create(amount=AmountRange.of(0), address=address),
         )
         for address in generate_n_address
     ]
     example = TreePay.create(payments=payments, radix=4)
-
-    from sapio_bitcoinlib.address import key_to_p2pkh, key_to_p2wpkh, script_to_p2wsh
-    from sapio_bitcoinlib.script import CScript
 
     alice_script = script_to_p2wsh(CScript([b"Alice's Key Goes Here!"]))
     bob_script = script_to_p2wsh(CScript([b"Bob's Key Goes Here!"]))
