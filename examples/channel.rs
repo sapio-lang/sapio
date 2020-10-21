e49759d9aef061eef4d32a43912afb5af2f9a70a
Jeremy Rubin <j@rubin.io>
2020-10-20 19:10:40 -0700

    clean up channel example

--- examples/channel.rs
+++ examples/channel.rs
@@ -13,6 +13,9 @@ use serde::{de::Visitor, Deserialize, Deserializer, Serialize, Serializer};
 use std::collections::HashMap;
 use std::fmt;
 use std::marker::PhantomData;
+use std::sync::{Arc, Mutex};
+
+use std::convert::{TryFrom, TryInto};
 
 fn main() {
     DB_serde::register_db("mock".to_string(), |s| Arc::new(Mutex::new(MockDB {})));
@@ -53,22 +56,7 @@ fn main() {
     Compilable::compile(&y);
 }
 
-#[derive(JsonSchema)]
-struct Start();
-#[derive(JsonSchema)]
-struct Stop();
-trait State {}
-impl State for Start {}
-impl State for Stop {}
-trait IsChannel<'a>
-where
-    Self: Sized + 'a,
-{
-    const begin_contest: Option<ThenFunc<'a, Self>> = None;
-    const finish_contest: Option<ThenFunc<'a, Self>> = None;
-}
-use std::sync::{Arc, Mutex};
-
+/// Args are some messages that can be passed to a Channel instance
 #[derive(Debug)]
 enum Args {
     Update {
@@ -76,9 +64,18 @@ enum Args {
         split: (Amount, Amount),
     },
 }
+
+/// Handle for DB Types
+#[derive(JsonSchema, Serialize, Deserialize)]
+struct DBHandle {
+    type_: String,
+    id: String,
+}
+/// DB Trait is for a Trait Object that can be used to record state updates for a channel.
+/// Examples implements a MockDB
 trait DB {
     fn save(&self, a: Args);
-    fn link(&self) -> String;
+    fn link(&self) -> DBHandle;
 }
 
 #[derive(JsonSchema)]
@@ -91,22 +88,22 @@ impl DB for MockDB {
             }
         }
     }
-    fn link(&self) -> String {
-        format!("mock:{}", "name")
+    fn link(&self) -> DBHandle {
+        DBHandle {
+            type_: "mock".into(),
+            id: "".into(),
+        }
     }
 }
 
+/// Custom Serialization Logic for DB Trait Critically, the method register_db can be used to add
+/// resolvers to get references to DB instances of arbitrary types.
 mod DB_serde {
     use super::*;
+    use serde::de::Error;
     use std::fmt::{self, Display};
 
     use lazy_static::lazy_static;
-    pub fn serialize<S>(db: &Arc<Mutex<dyn DB>>, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_str(&db.lock().unwrap().link())
-    }
     lazy_static! {
         static ref DB_TYPES: Mutex<HashMap<String, fn(&str) -> Arc<Mutex<dyn DB>>>> =
             Mutex::new(HashMap::new());
@@ -116,35 +113,36 @@ mod DB_serde {
         assert!(DB_TYPES.lock().unwrap().insert(s, f).is_none());
     }
 
-    struct StringVisitor;
-    impl<'de> Visitor<'de> for StringVisitor {
-        type Value = Arc<Mutex<dyn DB>>;
-        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-            formatter.write_str("A handle, depending on what's locally registered")
-        }
-        fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            if let Some(f) = DB_TYPES
-                .lock()
-                .unwrap()
-                .get(value.split(":").next().unwrap())
-            {
-                Ok(f(value))
-            } else {
-                Err(E::unknown_variant(value, &[]))
-            }
-        }
-    }
-
     pub fn deserialize<'de, D>(deserializer: D) -> Result<Arc<Mutex<dyn DB>>, D::Error>
     where
         D: Deserializer<'de>,
     {
-        deserializer.deserialize_str(StringVisitor)
+        let handle = DBHandle::deserialize(deserializer)?;
+        if let Some(f) = DB_TYPES.lock().unwrap().get(&handle.type_) {
+            Ok(f(&handle.id))
+        } else {
+            Err(D::Error::unknown_variant(&handle.type_, &[]))
+        }
+    }
+
+    pub fn serialize<S>(db: &Arc<Mutex<dyn DB>>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        db.lock().unwrap().link().serialize(serializer)
     }
 }
+
+/// The Different Operating States a Channel may be in.
+/// These States are enum'd at the trait/type level so as
+/// to be used as type tags
+trait State {}
+#[derive(JsonSchema)]
+struct Start();
+#[derive(JsonSchema)]
+struct Stop();
+impl State for Start {}
+impl State for Stop {}
 
 #[derive(JsonSchema, Serialize, Deserialize)]
 struct Channel<T: State> {
@@ -153,43 +151,13 @@ struct Channel<T: State> {
     bob: bitcoin::PublicKey,
     amount: CoinAmount,
     resolution: Compiled,
-    #[schemars(with = "String")]
+    /// We instruct the JSONSchema to use strings
+    #[schemars(with = "DBHandle")]
     #[serde(with = "DB_serde")]
     db: Arc<Mutex<dyn DB>>,
 }
 
-use std::convert::{TryFrom, TryInto};
-type Coin = Result<Amount, ParseAmountError>;
-impl<'a> IsChannel<'a> for Channel<Start> {
-    then! {begin_contest |s| {
-        Box::new(std::iter::once(
-                txn::TemplateBuilder::new()
-                .add_output(txn::Output::new(
-                        s.amount,
-                        Channel::<Stop> {
-                            pd: Default::default(),
-                            alice: s.alice,
-                            bob: s.bob,
-                            amount: s.amount.try_into().unwrap(),
-                            resolution: s.resolution.clone(),
-                            db: s.db.clone()
-                        },
-                        None,
-                ))
-                .into(),
-        ))
-    }
-    }
-}
-impl<'a> IsChannel<'a> for Channel<Stop> {
-    then! {finish_contest [Self::timeout] |s| {
-        Box::new(std::iter::once(
-                txn::TemplateBuilder::new()
-                .add_output(txn::Output::new(s.amount, s.resolution.clone(), None))
-                .into(),
-        ))
-    }}
-}
+/// Functionality Available for a channel regardless of state
 impl<'a, T: State + 'a> Channel<T> {
     guard!(timeout | s | { Clause::Older(100) });
     guard!(cached signed |s| {Clause::And(vec![Clause::Key(s.alice), Clause::Key(s.bob)])});
@@ -212,9 +180,54 @@ impl<'a, T: State + 'a> Channel<T> {
     }
 }
 
+/// Functionality that differs depending on current State
+trait FunctionalityAtState<'a>
+where
+    Self: Sized + 'a,
+{
+    const begin_contest: Option<ThenFunc<'a, Self>> = None;
+    const finish_contest: Option<ThenFunc<'a, Self>> = None;
+}
+
+/// Override begin_contest when state = Start
+impl<'a> FunctionalityAtState<'a> for Channel<Start> {
+    then! {begin_contest |s| {
+        Box::new(std::iter::once(
+                txn::TemplateBuilder::new()
+                .add_output(txn::Output::new(
+                        s.amount,
+                        Channel::<Stop> {
+                            pd: Default::default(),
+                            alice: s.alice,
+                            bob: s.bob,
+                            amount: s.amount.try_into().unwrap(),
+                            resolution: s.resolution.clone(),
+                            db: s.db.clone()
+                        },
+                        None,
+                ))
+                .into(),
+        ))
+    }
+    }
+}
+
+/// Override finish_contest when state = Start
+impl<'a> FunctionalityAtState<'a> for Channel<Stop> {
+    then! {finish_contest [Self::timeout] |s| {
+        Box::new(std::iter::once(
+                txn::TemplateBuilder::new()
+                .add_output(txn::Output::new(s.amount, s.resolution.clone(), None))
+                .into(),
+        ))
+    }}
+}
+
+/// Implement Contract for Channel<T> and functionality will be correctly assembled for different
+/// States.
 impl<'a, T: State + 'a> Contract<'a> for Channel<T>
 where
-    Channel<T>: IsChannel<'a>,
+    Channel<T>: FunctionalityAtState<'a>,
 {
     def! {then, Self::begin_contest, Self::finish_contest}
     def! {updatable<Args>, Self::update_state_a, Self::update_state_b }
