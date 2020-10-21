193bd99d07151a862ab2cfa69d05723164040954
Jeremy Rubin <j@rubin.io>
2020-10-20 19:51:11 -0700

    improve docs

--- src/clause.rs
+++ src/clause.rs
@@ -5,4 +5,3 @@ use lazy_static::lazy_static;
 /// Concrete Instantiation of Miniscript Policy. Because we need to be able to generate exact
 /// transactions, we only work with `bitcoin::PublicKey` types.
 pub type Clause = miniscript::policy::concrete::Policy<bitcoin::PublicKey>;
-
--- src/contract.rs
+++ src/contract.rs
@@ -9,7 +9,18 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-/// A struct which holds a compiled contract's context
+/// private::ImplSeal prevents anyone from implementing Compilable except by implementing Contract.
+mod private {
+    pub trait ImplSeal {}
+
+    /// Allow Contract to implement Compile
+    impl<T> ImplSeal for T where T: for<'a> super::Contract<'a> {}
+    impl ImplSeal for super::Compiled {}
+}
+
+/// Compiled holds a contract's complete context required post-compilation
+/// There is no guarantee that Compiled is properly constructed presently.
+//TODO: Make type immutable and correct by construction...
 #[derive(Serialize, Deserialize, JsonSchema, Clone)]
 pub struct Compiled {
     pub ctv_to_tx: HashMap<sha256::Hash, Template>,
@@ -19,6 +30,8 @@ pub struct Compiled {
 }
 
 impl Compiled {
+    /// converts a descriptor and an optional AmountRange to a compiled object.
+    /// This can be used for e.g. creating raw SegWit Scripts.
     pub fn from_descriptor(d: Descriptor<bitcoin::PublicKey>, a: Option<AmountRange>) -> Compiled {
         Compiled {
             ctv_to_tx: HashMap::new(),
@@ -34,55 +47,74 @@ impl Compiled {
     }
 }
 
+/// An Iterator which yields TransactionTemplates.
+/// It is boxed to permit flexibility when returning.
 pub type TxTmplIt<'a> = Box<dyn Iterator<Item = TransactionTemplate> + 'a>;
+
+/// A Guard is a function which generates some condition that must be met to unlock a script.
+/// If bool = true, the computation of the guard is cached, which is useful if e.g. Guard
+/// must contact a remote server or it should be the same across calls *for a given contract
+/// instance*.
 pub struct Guard<ContractSelf>(pub fn(&ContractSelf) -> Clause, pub bool);
 
+/// A List of Guards, for convenience
 pub type GuardList<'a, T> = &'a [Option<Guard<T>>];
 
+/// A ThenFunc takes a list of Guards and a TxTmplIt generator.  Each TxTmpl returned from the
+/// ThenFunc is Covenant Permitted only if the AND of all guards is satisfied.
 pub struct ThenFunc<'a, ContractSelf: 'a>(
     pub GuardList<'a, ContractSelf>,
     pub fn(&ContractSelf) -> TxTmplIt,
 );
 
-/// A function which by default finishes, but may receive some context object
-/// which can induce the generation of additional transactions (as a suggestion)
-
+/// A function which by default finishes, but may receive some context object which can induce the
+/// generation of additional transactions (as a suggestion)
+///
+/// FinishOrFuncNew is used to construct a FinishOrFunc to workaround the const_fn restrictions on
+/// function arguments.
 pub struct FinishOrFunc<'a, ContractSelf: 'a, Extra> {
     ffn: FinishOrFuncNew<'a, ContractSelf, Extra>,
 }
 
-/// Helper which creates a FinishOrFunc, a workaround of const_fn not accepting
-/// arguments that are fns
+/// Workaround of const_fn not accepting arguments that are fns, otherwise this would be inlined
+/// inside of FinishOrFunc.
 pub struct FinishOrFuncNew<'a, ContractSelf: 'a, Extra>(
     pub GuardList<'a, ContractSelf>,
     pub fn(&'a ContractSelf, Option<&'a Extra>) -> TxTmplIt<'a>,
 );
 
 impl<'a, ContractSelf: 'a, Extra> FinishOrFunc<'a, ContractSelf, Extra> {
+    /// Accessor to get the function of a FinishOrFunc
     fn fun(&self) -> fn(&'a ContractSelf, Option<&'a Extra>) -> TxTmplIt<'a> {
         self.ffn.1
     }
 
+    /// Accessor to get the guards of a FinishOrFunc
     fn guards(&self) -> &'a [Option<Guard<ContractSelf>>] {
         self.ffn.0
     }
 }
+
+/// Because From is a Trait, it cannot be const. Therefore we provide our own  non-trait method.
 impl<'a, ContractSelf: 'a, Extra> FinishOrFuncNew<'a, ContractSelf, Extra> {
-    pub const fn build(self) -> FinishOrFunc<'a, ContractSelf, Extra> {
+    pub const fn into(self) -> FinishOrFunc<'a, ContractSelf, Extra> {
         FinishOrFunc { ffn: self }
     }
 }
 
-pub trait Compilable {
+/// Compilable is a trait for anything which can be compiled
+pub trait Compilable: private::ImplSeal {
     fn compile(&self) -> Compiled;
 }
 
+/// Implements a basic identity
 impl Compilable for Compiled {
     fn compile(&self) -> Compiled {
         self.clone()
     }
 }
 
+/// The def macro is used to define the list of pathways in a contract
 #[macro_export]
 macro_rules! def {
     {then $(,$a:expr)*} => {
@@ -105,6 +137,8 @@ macro_rules! def {
 
 
 }
+
+/// The then macro is used to define a `ThenFunc`
 #[macro_export]
 macro_rules! then {
     {$name:ident $a:tt |$s:ident| $b:block } => {
@@ -113,15 +147,18 @@ macro_rules! then {
     {$name:ident |$s:ident| $b:block } => { then!{$name [] |$s| $b } };
 }
 
+/// The then macro is used to define a `FinishFunc` or a `FinishOrFunc`
 #[macro_export]
 macro_rules! finish {
     {$name:ident $a:tt |$s:ident, $o:ident| $b:block } => {
-        const $name: Option<FinishOrFunc<'a, Self, Args>> = Some(FinishOrFuncNew(&$a, |$s: &Self, $o: Option<&_>| $b) .build());
+        const $name: Option<FinishOrFunc<'a, Self, Args>> = Some(FinishOrFuncNew(&$a, |$s: &Self, $o: Option<&_>| $b) .into());
     };
     {$name:ident $a:tt} => {
         finish!($name $a |s, o| {Box::new(std::iter::empty())});
     };
 }
+
+/// The guard macro is used to define a `Guard`. Guards may be cached or uncached.
 #[macro_export]
 macro_rules! guard {
     {$name:ident |$s:ident| $b:block} => {
@@ -130,12 +167,7 @@ macro_rules! guard {
                                          };
     {cached $name:ident |$s:ident| $b:block} => { const $name: Option<Guard<Self>> = Some(Guard( |$s: &Self| $b, true,)); };
 }
-pub trait Contract<'a>
-where
-    Self: Sized + 'a,
-    Option<&'a Self::StatefulArguments>: Default,
-{
-    //! Main Contract Trait
+
 
     /// A catch-all type for any function that is a FinishOrFunc.
     /// Unfortunately, because type signatures must all match, it's not
@@ -143,21 +175,29 @@ where
     /// Use an enum if need be.
     ///
     /// TODO: use associated-type defaults here!
+pub trait Contract<'a>
+where
+    Self: Sized + 'a,
+    Option<&'a Self::StatefulArguments>: Default,
+{
+    //! Main Contract Trait
     def! {then}
     def! { updatable<> }
     def! {finish}
 }
 
-/// Sealed prevents anyone from implementing Compilable except by implementing Contract.
-mod seal {
-    pub trait Sealed {}
-}
-impl<T> seal::Sealed for T where T: for<'a> Contract<'a> {}
 impl<T> Compilable for T
 where
-    T: for<'a> Contract<'a> + seal::Sealed,
+    T: for<'a> Contract<'a>,
 {
+    /// The main Compilation Logic for a Contract.
+    /// TODO: Better Document Semantics
     fn compile(&self) -> Compiled {
+        #[derive(PartialEq, Eq)]
+        enum UsesCTV {
+            Yes,
+            No,
+        }
         // Evaluate all Guards One Time and store in a map
         let guard_clauses = {
             let mut guard_clauses: HashMap<usize, Clause> = HashMap::new();
@@ -256,10 +296,4 @@ where
             amount_range,
         };
     }
-}
-
-#[derive(PartialEq, Eq)]
-enum UsesCTV {
-    Yes,
-    No,
 }
