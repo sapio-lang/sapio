03950be636b752407bbef2e5d5c2a65f4522fb6a
Jeremy Rubin <j@rubin.io>
2020-10-23 10:53:59 -0700

    Move TreePay to own file, Create HodlChicken

---
+++ examples/server/contracts/hodl_chicken.rs
@@ -0,0 +1,118 @@
+/**
+* This License applies solely to the file hodl_chicken.rs.
+* Copyright (c) 2020, Pyskell and Judica, Inc
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the <organization> nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+use bitcoin::util::amount::CoinAmount;
+use sapio::clause::Clause;
+use sapio::contract::macros::*;
+use sapio::contract::*;
+use sapio::*;
+use schemars::*;
+use serde::*;
+use std::collections::HashMap;
+use std::convert::TryFrom;
+
+#[derive(JsonSchema, Serialize, Deserialize)]
+#[serde(try_from = "HodlChickenChecks")]
+pub struct HodlChickenInner {
+    alice_contract: HashMap<u64, Compiled>,
+    bob_contract: HashMap<u64, Compiled>,
+    alice_key: bitcoin::PublicKey,
+    bob_key: bitcoin::PublicKey,
+    alice_deposit: u64,
+    bob_deposit: u64,
+    winner_gets: u64,
+    chicken_gets: u64,
+}
+
+#[derive(JsonSchema, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct HodlChickenChecks(HodlChickenInner);
+
+impl TryFrom<HodlChickenChecks> for HodlChickenInner {
+    type Error = &'static str;
+    fn try_from(a: HodlChickenChecks) -> Result<Self, Self::Error> {
+        let inner = a.0;
+        let deposits = inner.alice_deposit.checked_add(inner.bob_deposit);
+        let outputs = inner.winner_gets.checked_add(inner.chicken_gets);
+        if deposits != outputs {
+            Err("Outputs not Equal Deposits")
+        } else if deposits == None {
+            Err("Amounts Overflow")
+        } else if inner.alice_deposit != inner.bob_deposit {
+            Err("Amounts differ")
+        } else if [&inner.alice_contract, &inner.bob_contract]
+            .iter()
+            .any(|c| {
+                [c.get(&inner.chicken_gets), c.get(&inner.winner_gets)]
+                    .iter()
+                    .any(Option::is_none)
+            })
+        {
+            Err("Map Incomplete")
+        } else {
+            Ok(inner)
+        }
+    }
+}
+
+impl<'a> HodlChickenInner {
+    guard! {alice_is_a_chicken |s| {Clause::Key(s.alice_key)}}
+    guard! {bob_is_a_chicken |s| {Clause::Key(s.bob_key)}}
+    then! {alice_redeem [Self::alice_is_a_chicken] |s| {
+        Ok(Box::new(std::iter::once(Ok(txn::TemplateBuilder::new()
+            .add_output(txn::Output::new(
+                CoinAmount::Sats(s.winner_gets),
+                s.bob_contract.get(&s.winner_gets).unwrap().clone(),
+                None,
+            )?)
+            .add_output(txn::Output::new(
+                CoinAmount::Sats(s.chicken_gets),
+                s.alice_contract.get(&s.chicken_gets).unwrap().clone(),
+                None,
+            )?)
+            .into()))))
+    }}
+
+    then! {bob_redeem [Self::bob_is_a_chicken] |s| {
+        Ok(Box::new(std::iter::once(Ok(txn::TemplateBuilder::new()
+            .add_output(txn::Output::new(
+                CoinAmount::Sats(s.winner_gets),
+                s.alice_contract.get(&s.winner_gets).unwrap().clone(),
+                None,
+            )?)
+            .add_output(txn::Output::new(
+                CoinAmount::Sats(s.chicken_gets),
+                s.bob_contract.get(&s.chicken_gets).unwrap().clone(),
+                None,
+            )?)
+            .into()))))
+    }}
+}
+
+pub struct Args;
+impl<'a> Contract<'a> for HodlChickenInner {
+    def! {then, Self::alice_redeem, Self::bob_redeem}
+    def! {updatable<Args>}
+}
