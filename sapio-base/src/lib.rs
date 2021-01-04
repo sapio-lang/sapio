97ef633fdfaf740f47eefcf61c707172598bed7e
Jeremy Rubin <j@rubin.io>
2021-01-04 15:40:31 -0800

    Unify Clause type aliases

--- sapio-base/src/lib.rs
+++ sapio-base/src/lib.rs
@@ -1,5 +1,9 @@
 pub mod util;
 pub use util::CTVHash;
+
+/// Concrete Instantiation of Miniscript Policy. Because we need to be able to generate exact
+/// transactions, we only work with `bitcoin::PublicKey` types.
+pub type Clause = miniscript::policy::concrete::Policy<bitcoin::PublicKey>;
 #[cfg(test)]
 mod tests {
     #[test]
