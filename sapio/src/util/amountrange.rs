111ac0454f85a66f6f2e26474036505afbc30845
Jeremy Rubin <j@rubin.io>
2021-01-13 10:42:14 -0800

    Fix AmountRange Serialization Bug Locally

--- sapio/src/util/amountrange.rs
+++ sapio/src/util/amountrange.rs
@@ -1,26 +1,46 @@
+use bitcoin::util::amount;
 use bitcoin::util::amount::{Amount, CoinAmount};
+use bitcoin::SignedAmount;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use std::convert::TryInto;
 
+#[derive(Serialize, Deserialize, JsonSchema, Clone, Copy, Debug, Ord, PartialOrd, PartialEq, Eq)]
+#[serde(transparent)]
+struct AmountF64(
+    #[schemars(with = "f64")]
+    #[serde(with = "bitcoin::util::amount::serde::as_btc")]
+    Amount,
+);
+
+impl From<Amount> for AmountF64 {
+    fn from(a: Amount) -> AmountF64 {
+        AmountF64(a)
+    }
+}
+impl From<AmountF64> for Amount {
+    fn from(a: AmountF64) -> Amount {
+        a.0
+    }
+}
+
 #[derive(Serialize, Deserialize, JsonSchema, Clone, Copy, Debug)]
 pub struct AmountRange {
-    min: CoinAmount,
-    max: CoinAmount,
+    min: Option<AmountF64>,
+    max: Option<AmountF64>,
 }
 impl AmountRange {
     pub fn new() -> AmountRange {
         AmountRange {
-            min: Amount::max_value().into(),
-            max: Amount::min_value().into(),
+            min: None,
+            max: None,
         }
     }
     pub fn update_range(&mut self, amount: Amount) {
-        // This is safe even though unwrap becuase min cannot be manually set to a nonsense value.
-        self.min = std::cmp::min(self.min.try_into().unwrap(), amount).into();
-        self.max = std::cmp::max(self.max.try_into().unwrap(), amount).into();
+        self.min = std::cmp::min(self.min, Some(amount.into()));
+        self.max = std::cmp::max(self.max, Some(amount.into()));
     }
-    pub fn max(&self) -> CoinAmount {
-        self.max
+    pub fn max(&self) -> Amount {
+        self.max.unwrap_or(Amount::min_value().into()).0
     }
 }
