fd4e44c43a465c6b04d563da4ac3139d247de337
Jeremy Rubin <j@rubin.io>
2021-01-13 18:57:08 -0800

    Add block in place inside of trait objects

--- sapio-base/src/txindex.rs
+++ sapio-base/src/txindex.rs
@@ -95,17 +95,21 @@ pub struct BitcoinNodeIndex {
 
 impl TxIndex for BitcoinNodeIndex {
     fn lookup_tx(&self, b: &Txid) -> Result<Arc<bitcoin::Transaction>> {
-        self.runtime
-            .block_on(self.client.get_raw_transaction(b, None))
-            .map(Arc::new)
-            .map_err(TxIndexError::RpcError)
+        tokio::task::block_in_place(|| {
+            self.runtime
+                .block_on(self.client.get_raw_transaction(b, None))
+                .map(Arc::new)
+                .map_err(TxIndexError::RpcError)
+        })
     }
     fn add_tx(&self, tx: Arc<bitcoin::Transaction>) -> Result<Txid> {
         let txid = tx.txid();
         if self.can_add {
-            self.runtime
-                .block_on(self.client.send_raw_transaction(&*tx))
-                .map_err(TxIndexError::RpcError)
+            tokio::task::block_in_place(|| {
+                self.runtime
+                    .block_on(self.client.send_raw_transaction(&*tx))
+                    .map_err(TxIndexError::RpcError)
+            })
         } else {
             Ok(txid)
         }
