d69303939769c89024ede0eb50cdcdcdf3039a15
Jeremy Rubin <j@rubin.io>
2021-02-24 11:51:15 -0800

    Split sapio-contrib/src/contracts/mod.rs

--- sapio-contrib/src/contracts/basic_examples.rs
+++ sapio-contrib/src/contracts/basic_examples.rs
@@ -1,20 +1,4 @@
-use bitcoin::util::amount::CoinAmount;
-use sapio_base::Clause;
-
-use sapio::contract::*;
-use sapio::*;
-use schemars::*;
-use serde::*;
-use std::convert::TryInto;
-pub mod derivatives;
-pub mod dynamic;
-pub mod federated_sidechain;
-pub mod hodl_chicken;
-pub mod readme_contracts;
-pub mod treepay;
-pub mod undo_send;
-pub mod vault;
-pub mod basic_examples;
+use super::*;
 
 #[derive(JsonSchema, Serialize, Deserialize)]
 pub struct ExampleA {
