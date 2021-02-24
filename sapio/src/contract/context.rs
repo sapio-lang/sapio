a8f900dd0826f88d1822b801c17c4154ca260f7c
Jeremy Rubin <j@rubin.io>
2021-02-23 21:58:59 -0800

    Add some docs to sapio

--- sapio/src/contract/context.rs
+++ sapio/src/contract/context.rs
@@ -6,6 +6,7 @@ use miniscript::DescriptorTrait;
 use sapio_ctv_emulator_trait::{CTVEmulator, NullEmulator};
 use std::collections::HashMap;
 use std::sync::Arc;
+/// Context is used to track statet during compilation such as remaining value.
 /// Context type is not copyable/clonable externally
 pub struct Context {
     /* TODO: Add Context Fields! */
@@ -15,6 +16,7 @@ pub struct Context {
 }
 
 impl Context {
+    /// create a context instance
     pub fn new(network: Network, amount: Amount, emulator: Option<Arc<dyn CTVEmulator>>) -> Self {
         Context {
             available_funds: amount,
@@ -22,19 +24,27 @@ impl Context {
             network,
         }
     }
+
+    /// return the available funds
     pub fn funds(&self) -> Amount {
         self.available_funds
     }
+
+    /// use the context's emulator to get a emulated (or not) clause
     pub fn ctv_emulator(
         &self,
         b: bitcoin::hashes::sha256::Hash,
     ) -> Result<sapio_base::Clause, CompilationError> {
         Ok(self.emulator.get_signer_for(b)?)
     }
+
+    /// Compile the compilable item with this context.
     pub fn compile<A: Compilable>(&self, a: A) -> Result<Compiled, CompilationError> {
         a.compile(&self)
     }
+
     // TODO: Fix
+    /// return a context with the new amount if amount is smaller or equal to available
     pub fn with_amount(&self, amount: Amount) -> Result<Self, CompilationError> {
         if self.available_funds < amount {
             Err(CompilationError::OutOfFunds)
@@ -46,6 +56,7 @@ impl Context {
             })
         }
     }
+    /// decrease the amount available in this context object.
     pub fn spend_amount(&mut self, amount: Amount) -> Result<(), CompilationError> {
         if self.available_funds < amount {
             Err(CompilationError::OutOfFunds)
@@ -55,10 +66,12 @@ impl Context {
         }
     }
 
+    /// Add funds to the context object (not typically needed)
     pub fn add_amount(&mut self, amount: Amount) {
         self.available_funds += amount;
     }
 
+    /// Get a template builder from this context object
     pub fn template(&self) -> crate::template::Builder {
         crate::template::Builder::new(Context {
             emulator: self.emulator.clone(),
