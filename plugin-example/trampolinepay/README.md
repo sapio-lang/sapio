7480be3ee3e6810798aac1ba2d4388ec85c700e2
Jeremy Rubin <j@rubin.io>
2021-08-17 01:46:42 -0700

    Trampoline Payment Example

---
+++ plugin-example/trampolinepay/README.md
@@ -0,0 +1,8 @@
+# Trampoline Pay
+
+This crate can be compiled with `wasm-pack build`. The `*.wasm` artifact will
+be created in the `pkg` directory, not in `target`.
+
+Feel free to modify this code to experiment with creating your own Sapio plugins.
+
+Trampoline Pay demonstrates using the Traits API.
---
+++ plugin-example/trampolinepay/src/plugin.rs
@@ -0,0 +1,63 @@
+// Copyright Judica, Inc 2021
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#[deny(missing_docs)]
+use batching_trait::BatchingTraitVersion0_1_1;
+use bitcoin::util::amount::Amount;
+use sapio::contract::*;
+use sapio::util::amountrange::*;
+use sapio::*;
+use sapio_wasm_plugin::client::*;
+use sapio_wasm_plugin::*;
+use schemars::*;
+use serde::*;
+use std::collections::VecDeque;
+
+/// Documentation placed here will be visible to users!
+#[derive(JsonSchema, Serialize, Deserialize)]
+pub struct TrampolinePay {
+    /// # Which Plugin to Use
+    /// Specify which contract plugin to call out to.
+    handle: SapioHostAPI<BatchingTraitVersion0_1_1>,
+    /// # Data for the Contract
+    // Just do this to get the data... not always necessary (could be computed any way)
+    data: BatchingTraitVersion0_1_1,
+}
+
+#[derive(Serialize, Deserialize, JsonSchema)]
+enum Versions {
+    /// # Batching Trait API
+    BatchingTraitVersion0_1_1(BatchingTraitVersion0_1_1),
+}
+impl TrampolinePay {
+    then! {
+        fn expand(self, ctx) {
+            let compiled = create_contract_by_key(
+                &self.handle.key,
+                serde_json::to_value(CreateArgs {
+                    amount: ctx.funds(),
+                    network: ctx.network,
+                    arguments: Versions::BatchingTraitVersion0_1_1(self.data.clone()),
+                })
+                .map_err(|_| CompilationError::TerminateCompilation)?,
+                Amount::from_sat(0),
+            );
+            if let Some(contract) = compiled {
+                let mut builder = ctx.template();
+                builder = builder.add_output(contract.amount_range.max(), &contract, None)?;
+                builder.into()
+            } else {
+                Err(CompilationError::TerminateCompilation)
+            }
+        }
+    }
+}
+impl Contract for TrampolinePay {
+    declare! {then, Self::expand}
+    declare! {non updatable}
+}
+
+REGISTER![TrampolinePay, "logo.png"];
