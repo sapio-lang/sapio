dc75d41ec7d666e7347ba8cd3cf8473d9951e03b
Jeremy Rubin <j@rubin.io>
2021-09-28 15:25:50 -0700

    Enable Serialization of Arc for RootSchema (generally, not able to serialize Arc unless a system-wide feature flag is set

--- sapio/src/contract/abi/continuation.rs
+++ sapio/src/contract/abi/continuation.rs
@@ -15,5 +15,27 @@ use std::sync::Arc;
 pub struct ContinuationPoint {
     /// The arguments required at this point
     /// TODO: De-Duplicate repeated types?
+    #[serde(serialize_with = "rs::serializer")]
+    #[serde(deserialize_with = "rs::deserializer")]
     pub schema: Arc<RootSchema>,
+}
+
+mod rs {
+    use schemars::schema::RootSchema;
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+    use std::borrow::Borrow;
+    use std::sync::Arc;
+    pub fn serializer<S>(v: &Arc<RootSchema>, s: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let b: &RootSchema = v.borrow();
+        b.serialize(s)
+    }
+    pub fn deserializer<'de, D>(d: D) -> Result<Arc<RootSchema>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Ok(Arc::new(RootSchema::deserialize(d)?))
+    }
 }
