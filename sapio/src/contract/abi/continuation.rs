bc5358e98aac1c546938c34261daa1af2cf0dbee
Jeremy Rubin <j@rubin.io>
2021-09-30 18:42:08 -0700

    continuation serialization fixes

--- sapio/src/contract/abi/continuation.rs
+++ sapio/src/contract/abi/continuation.rs
@@ -12,23 +12,42 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 /// Instructions for how to resume a contract compilation at a given point
-#[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
+#[derive(Serialize, Deserialize, JsonSchema, Clone, Debug, PartialEq)]
 pub struct ContinuationPoint {
     /// The arguments required at this point
     /// TODO: De-Duplicate repeated types?
-    #[serde(serialize_with = "rs::serializer")]
-    #[serde(deserialize_with = "rs::deserializer")]
-    pub schema: Arc<RootSchema>,
+    pub schema: Option<rs::SArc<RootSchema>>,
     /// The path at which this was compiled
     #[serde(serialize_with = "rs::serializer")]
     #[serde(deserialize_with = "rs::deserializer")]
     pub path: Arc<ReversePath<String>>,
 }
+impl ContinuationPoint {
+    /// Creates a new continuation
+    pub fn at(schema: Option<Arc<RootSchema>>, path: Arc<ReversePath<String>>) -> Self {
+        ContinuationPoint {
+            schema: schema.map(rs::SArc),
+            path,
+        }
+    }
+}
 
 mod rs {
+    use schemars::JsonSchema;
     use serde::{Deserialize, Deserializer, Serialize, Serializer};
     use std::borrow::Borrow;
     use std::sync::Arc;
+
+    #[derive(Serialize, Deserialize, JsonSchema, Clone, Debug, PartialEq, PartialOrd)]
+    #[serde(
+        bound = "T: Serialize + for<'d> Deserialize<'d> + JsonSchema + std::fmt::Debug + Clone "
+    )]
+    #[serde(transparent)]
+    pub struct SArc<T>(
+        #[serde(serialize_with = "serializer")]
+        #[serde(deserialize_with = "deserializer")]
+        pub Arc<T>,
+    );
     pub fn serializer<T, S>(v: &Arc<T>, s: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
@@ -43,5 +62,29 @@ mod rs {
         T: Deserialize<'de>,
     {
         Ok(Arc::new(T::deserialize(d)?))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_sarc_ser() -> Result<(), Box<dyn std::error::Error>> {
+        assert_eq!(serde_json::to_string(&rs::SArc(Arc::new(1)))?, "1");
+        Ok(())
+    }
+
+    #[test]
+    fn test_continuation_point_ser() -> Result<(), Box<dyn std::error::Error>> {
+        let a: ContinuationPoint = ContinuationPoint::at(
+            Some(Arc::new(schemars::schema_for!(ContinuationPoint))),
+            ReversePath::push(None, Arc::new("one".into())),
+        );
+        let b: ContinuationPoint = serde_json::from_str(&format!(
+            "{{\"schema\":{},\"path\":[\"one\"]}}",
+            serde_json::to_string(&schemars::schema_for!(ContinuationPoint))?
+        ))?;
+        assert_eq!(a, b);
+        Ok(())
     }
 }
