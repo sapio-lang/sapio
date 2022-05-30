4d7375a2687f328a4c9ec687dcc21402291108a8
Jeremy Rubin <j@rubin.io>
2022-05-29 17:56:14 -0700

    [Compiler] Separate out taproot processing code.
    Extracts two functions for converting branches to a taproot tree and for
    picking a key out of a list of branches from the main compiler logic.

--- sapio/src/contract/compiler/mod.rs
+++ sapio/src/contract/compiler/mod.rs
@@ -321,37 +321,9 @@ where
                 .collect::<Result<Vec<_>, _>>()?
         };
         // TODO: Pick a better branch that is guaranteed to work!
-        let some_key = branches
-            .iter()
-            .filter_map(|f| {
-                if let Terminal::Check(check) = &f.node {
-                    if let Terminal::PkK(k) = &check.node {
-                        return Some(k.clone());
-                    }
-                }
-                None
-            })
-            .next()
-            .map(|x| bitcoin::util::schnorr::UntweakedPublicKey::from(x))
-            .unwrap_or(
-                XOnlyPublicKey::from_slice(&Sha256::hash(&[1u8; 32]).into_inner())
-                    .expect("constant"),
-            );
+        let some_key = pick_key_from_miniscripts(branches.iter());
         // Don't remove the key from the scripts in case it was bogus
-        let mut scripts: BinaryHeap<(Reverse<u64>, TapTree<XOnlyPublicKey>)> = branches
-            .iter()
-            .map(|b| (Reverse(1), TapTree::Leaf(Arc::new(b.clone()))))
-            .collect();
-        while scripts.len() > 1 {
-            let (w1, v1) = scripts.pop().unwrap();
-            let (w2, v2) = scripts.pop().unwrap();
-            scripts.push((
-                Reverse(w1.0.saturating_add(w2.0)),
-                TapTree::Tree(Arc::new(v1), Arc::new(v2)),
-            ));
-        }
-
-        let tree = scripts.pop().map(|v| v.1);
+        let tree = branches_to_tree(branches);
         let descriptor = Descriptor::Tr(descriptor::Tr::new(some_key, tree)?);
         let estimated_max_size = descriptor.max_satisfaction_weight()?;
         let address = descriptor.address(ctx.network)?.into();
@@ -382,4 +354,43 @@ where
             })
         }
     }
+}
+
+/// picks a key from an iter of miniscripts, or returns a static default key
+fn pick_key_from_miniscripts<'a, I: Iterator<Item = &'a Miniscript<XOnlyPublicKey, Tap>>>(
+    branches: I,
+) -> XOnlyPublicKey {
+    branches
+        .filter_map(|f| {
+            if let Terminal::Check(check) = &f.node {
+                if let Terminal::PkK(k) = &check.node {
+                    return Some(k.clone());
+                }
+            }
+            None
+        })
+        .next()
+        .map(|x| bitcoin::util::schnorr::UntweakedPublicKey::from(x))
+        .unwrap_or(
+            XOnlyPublicKey::from_slice(&Sha256::hash(&[1u8; 32]).into_inner()).expect("constant"),
+        )
+}
+
+/// Convert the branches into a heap for taproot tree consumption
+fn branches_to_tree(
+    branches: Vec<Miniscript<XOnlyPublicKey, Tap>>,
+) -> Option<TapTree<XOnlyPublicKey>> {
+    let mut scripts: BinaryHeap<(Reverse<u64>, TapTree<XOnlyPublicKey>)> = branches
+        .into_iter()
+        .map(|b| (Reverse(1), TapTree::Leaf(Arc::new(b))))
+        .collect();
+    while scripts.len() > 1 {
+        let (w1, v1) = scripts.pop().unwrap();
+        let (w2, v2) = scripts.pop().unwrap();
+        scripts.push((
+            Reverse(w1.0.saturating_add(w2.0)),
+            TapTree::Tree(Arc::new(v1), Arc::new(v2)),
+        ));
+    }
+    scripts.pop().map(|v| v.1)
 }
