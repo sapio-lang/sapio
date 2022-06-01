aa6877f7b624bf27885c6a9a056c42963f93fee6
Jeremy Rubin <j@rubin.io>
2022-06-01 16:19:26 -0700

    [Tests] don't specify clang

--- contrib/sapio_wasm.sh
+++ contrib/sapio_wasm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-CC=clang-9 cargo build --release --target wasm32-unknown-unknown --manifest-path plugin-example/Cargo.toml
+cargo build --release --target wasm32-unknown-unknown --manifest-path plugin-example/Cargo.toml
 
 CLAUSE="$(cat contrib/vectors/clause_input.json | ./target/release/sapio-cli --config contrib/vectors/basic_config.json contract create --file plugin-example/target/wasm32-unknown-unknown/release/sapio_wasm_clause.wasm)"
 EXPECTED="$(cat contrib/vectors/clause_output.json)"
