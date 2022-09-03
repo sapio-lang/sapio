6f9faa1598abc92b85099294efe7f41ea9e5ce07
Jeremy Rubin <j@rubin.io>
2022-09-03 16:34:10 -0700

    Add SIMP to ContinuationPoint (not wired up for users

--- sapio/src/contract/abi/continuation.rs
+++ sapio/src/contract/abi/continuation.rs
@@ -6,12 +6,14 @@
 
 //! ABI for contract resumption
 
-use sapio_base::effects::EffectPath;
 use sapio_base::serialization_helpers::SArc;
+use sapio_base::simp::{SIMPAttachableAt, SIMPError};
+use sapio_base::{effects::EffectPath, simp::ContinuationPointLT};
 use schemars::schema::RootSchema;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use std::sync::Arc;
+use serde_json::Value;
+use std::{collections::BTreeMap, sync::Arc};
 /// Instructions for how to resume a contract compilation at a given point
 #[derive(Serialize, Deserialize, JsonSchema, Clone, Debug, PartialEq)]
 pub struct ContinuationPoint {
@@ -22,6 +24,8 @@ pub struct ContinuationPoint {
     #[serde(serialize_with = "sapio_base::serialization_helpers::serializer")]
     #[serde(deserialize_with = "sapio_base::serialization_helpers::deserializer")]
     pub path: Arc<EffectPath>,
+    /// Metadata for this particular Continuation Point
+    pub simp: BTreeMap<i64, Value>,
 }
 impl ContinuationPoint {
     /// Creates a new continuation
@@ -29,6 +33,24 @@ impl ContinuationPoint {
         ContinuationPoint {
             schema: schema.map(SArc),
             path,
+            simp: Default::default(),
+        }
+    }
+
+    /// attempts to add a SIMP to the output meta.
+    ///
+    /// Returns [`SIMPError::AlreadyDefined`] if one was previously set.
+    pub fn add_simp<S: SIMPAttachableAt<ContinuationPointLT>>(
+        mut self,
+        s: S,
+    ) -> Result<Self, SIMPError> {
+        let old = self
+            .simp
+            .insert(S::get_protocol_number(), serde_json::to_value(&s)?);
+        if let Some(old) = old {
+            Err(SIMPError::AlreadyDefined(old))
+        } else {
+            Ok(self)
         }
     }
 }
@@ -44,7 +66,7 @@ mod test {
             EffectPath::push(None, PathFragment::Named(SArc(Arc::new("one".into())))),
         );
         let b: ContinuationPoint = serde_json::from_str(&format!(
-            "{{\"schema\":{},\"path\":\"one\"}}",
+            "{{\"schema\":{},\"path\":\"one\",\"simp\":{{}}}}",
             serde_json::to_string(&schemars::schema_for!(ContinuationPoint))?
         ))?;
         assert_eq!(a, b);
