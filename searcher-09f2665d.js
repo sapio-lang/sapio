5b2ac93242e79f63f84dded0d1c61e3c0ac8a7b9
JeremyRubin <JeremyRubin@users.noreply.github.com>
2026-09-09 21:20:53 +0000

    deploy: 582cbb270408ed69f1c956f0a791ac8ad0e57061

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-3b445493.js',
+                path_to_root + 'searchindex-3ae5aa73.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
